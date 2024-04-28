e0d1d465cd86ccba10555963640fdec795700fc3
rin <rinnn418@gmail.com>
2024-04-28 17:39:04 +0200

    fix: create stores folder

--- src/hooks/useJwtLogin.ts
+++ src/hooks/useJwtLogin.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
-import { useUserStore } from "../pages/assets/store";
+import { useUserStore } from "../stores/UserStore";
 import { useEffect } from "react";
 
 export default function useJwtLogin() {
