06ee2cf503be7a629b6c5ab0880f2cf0f673d3d7
rin <rinnn418@gmail.com>
2024-04-28 17:26:00 +0200

    feat: making app layout

---
+++ src/hooks/useJwtLogin.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { useUserStore } from "../pages/assets/store";
+import { useEffect } from "react";
+
+export default function useJwtLogin() {
+  const { isLoading, data, isSuccess, isError, error } = useQuery<{ username: string }, AxiosError<{ message: string }>>({
+    queryKey: ['jwt-login'],
+    queryFn: async () => {
+      const response = await axios.get<{ username: string }>('http://localhost:3000/auth/login', { withCredentials: true });
+      return response.data;
+    },
+    retry: 1
+  })
+
+  const { setUsername } = useUserStore()
+  
+  useEffect(() => {
+    if (isLoading) return
+
+    if (isSuccess) {
+      setUsername(data.username)
+    } else {
+      setUsername('')
+    }
+  }, [isLoading]);
+
+  return { isSuccess, isError, error, isLoading }
+}
